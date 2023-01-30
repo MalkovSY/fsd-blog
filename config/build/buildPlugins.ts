dc01fb0ffed6558ab5aa93047758173a3c22212e
Малков Сергей Юрьевич <smalkov@phoenixit.ru>
2023-01-30 19:29:24 +0300

    fix: fix path

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,6 @@
 import { ProgressPlugin, WebpackPluginInstance } from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import {BuildOptions} from "./types/config";
+import { BuildOptions } from "./types/config";
 
 export function buildPlugins(options: BuildOptions): Array<WebpackPluginInstance> {
     const { paths: { html } } = options;
