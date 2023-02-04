0e391647a4c45be14f54da4f306a22dd535219d1
Малков Сергей Юрьевич <smalkov@phoenixit.ru>
2023-02-04 14:45:53 +0300

    fix: install react-router-dom, first routes

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -6,6 +6,7 @@ export function buildDevServer(options: BuildOptions): DevServerConfiguration {
 
     return {
         port,
-        open: true
+        open: true,
+        historyApiFallback: true
     }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import {Counter} from "./components/Counter";
 import './index.scss';
+import { Link, Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage/MainPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
 
 const App = () => {
     return (
         <div className='app'>
-            <Counter/>
+            <Link to='/main'>main</Link>
+            <Link to='/about'>about</Link>
+            <Routes>
+                <Route path={'/main'} element={<MainPage/>}/>
+                <Route path={'/about'} element={<AboutPage/>}/>
+            </Routes>
         </div>
     );
 };
 
 export default App;
+
