a1718076ce60ac2825608f31355e33dbb66db029
Малков Сергей Юрьевич <smalkov@phoenixit.ru>
2023-02-04 19:38:03 +0300

    fix: code splitting with react lazy and suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import './index.scss';
+import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import MainPage from "./pages/MainPage/MainPage";
-import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPageAsync from "./pages/MainPage/MainPage.async";
+import AboutPageAsync from "./pages/AboutPage/AboutPage.async";
+import './index.scss';
 
 const App = () => {
     return (
         <div className='app'>
             <Link to='/main'>main</Link>
             <Link to='/about'>about</Link>
-            <Routes>
-                <Route path={'/main'} element={<MainPage/>}/>
-                <Route path={'/about'} element={<AboutPage/>}/>
-            </Routes>
+            <Suspense fallback={'...loading'}>
+                <Routes>
+                    <Route path={'/main'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
