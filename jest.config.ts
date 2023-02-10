7fbd1707b149842be83b2ea764e95ba31c2abce0
Малков Сергей Юрьевич <smalkov@phoenixit.ru>
2023-02-10 16:26:55 +0300

    install jest, config jest, test on classNames func

---
+++ jest.config.ts
@@ -0,0 +1,31 @@
+export default {
+    clearMocks: true,
+    collectCoverage: false,
+    coverageDirectory: 'coverage',
+    coveragePathIgnorePatterns: [
+        '\\\\node_modules\\\\',
+    ],
+    coverageProvider: 'babel',
+    coverageReporters: [
+        'json',
+        'text',
+        'lcov',
+        'clover',
+    ],
+    moduleDirectories: ['node_modules'],
+    testEnvironment: 'jsdom',
+    testMatch: [
+        '<rootDir>/src/**/*.(spec|test).[tj]s?(x)',
+    ],
+    watchPathIgnorePatterns: ['node_modules'],
+    moduleFileExtensions: [
+        'js',
+        'mjs',
+        'cjs',
+        'jsx',
+        'ts',
+        'tsx',
+        'json',
+        'node',
+    ],
+};
---
+++ src/shared/lib/classNames/classNames.spec.ts
@@ -0,0 +1,33 @@
+import { classNames } from './classNames';
+
+describe('classNames tests', () => {
+    it('with first arg return this string', () => {
+        const classString = classNames('firstClass');
+
+        expect(classString).toBe('firstClass');
+    });
+
+    it('with additional prop return joined string', () => {
+        const classString = classNames('firstClass', {}, ['secondClass']);
+
+        expect(classString).toBe('firstClass secondClass');
+    });
+
+    it('with mods prop return joined string with mod keys', () => {
+        const classString = classNames('firstClass', { hovered: true, secondary: true }, ['secondClass']);
+
+        expect(classString).toBe('firstClass secondClass hovered secondary');
+    });
+
+    it('if mod item falsy - not pushed into joined string', () => {
+        const classString = classNames('firstClass', { hovered: true, secondary: false }, ['secondClass']);
+
+        expect(classString).toBe('firstClass secondClass hovered');
+    });
+
+    it('if mod item undefined - not pushed into joined string', () => {
+        const classString = classNames('firstClass', { hovered: undefined, secondary: false }, ['secondClass']);
+
+        expect(classString).toBe('firstClass secondClass');
+    });
+});
