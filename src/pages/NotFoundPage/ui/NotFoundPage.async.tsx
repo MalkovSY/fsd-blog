beff0074cfaacf64f57108cafced896b3543c02a
Малков Сергей Юрьевич <smalkov@phoenixit.ru>
2023-02-13 16:03:21 +0300

    delete comment

--- src/pages/NotFoundPage/ui/NotFoundPage.async.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.async.tsx
@@ -1,5 +1,8 @@
 import { lazy } from 'react';
 
-const NotFoundPageAsync = lazy(() => import('./NotFoundPage'));
+export const NotFoundPageAsync = lazy(() => new Promise((resolve) => {
+    // @ts-ignore
+    setTimeout(() => resolve(import('./NotFoundPage')), 5000);
+}));
 
 export default NotFoundPageAsync;
