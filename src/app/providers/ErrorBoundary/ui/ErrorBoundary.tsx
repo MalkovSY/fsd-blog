9c39f0b0e1a82c6a2bc2191641851db49f6c9a96
Малков Сергей Юрьевич <smalkov@phoenixit.ru>
2023-02-14 17:05:41 +0300

    implemented error boundary component

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,49 @@
+import { Component, ReactNode } from 'react';
+import { withTranslation, WithTranslation } from 'react-i18next';
+
+import './ErrorBoundary.scss';
+
+interface ErrorBoundaryProps extends WithTranslation {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  error: { message: string, stack: string } | null;
+}
+
+class ErrorBoundary
+  extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { error: null };
+  }
+
+  componentDidCatch(error:Error) {
+    return this.setState({
+      error: {
+        message: error.message,
+        stack: error.stack,
+      },
+    });
+  }
+
+  render() {
+    const { error } = this.state;
+    const { children, t } = this.props;
+
+    if (error) {
+      return (
+        <div className="ErrorBoundary">
+          <h1>{t('Что-то пошло не так')}</h1>
+          <h3>{error.message}</h3>
+          <p>{t('Подробности')}</p>
+          <p className="errorBoundaryStack">{error.stack}</p>
+        </div>
+      );
+    }
+
+    return children;
+  }
+}
+
+export default withTranslation()(ErrorBoundary);
--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,20 @@ import { Suspense } from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import App from './App/App';
 
 import 'shared/config/i18nConfig/i18nConfig';
 
 render(
   <BrowserRouter>
-    <ThemeProvider>
-      <Suspense fallback="translate...">
-        <App />
-      </Suspense>
-    </ThemeProvider>
+    <ErrorBoundary useSuspense={false}>
+      <ThemeProvider>
+        <Suspense fallback="Перевод...">
+          <App />
+        </Suspense>
+      </ThemeProvider>
+    </ErrorBoundary>
   </BrowserRouter>,
   document.getElementById('root'),
 );
