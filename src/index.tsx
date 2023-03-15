70764f3d861799c8b4c601be288e3f66c3f32a87
Малков Сергей Юрьевич <smalkov@phoenixit.ru>
2023-03-15 18:42:46 +0300

    create StoreProvider, wrap app

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,22 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
+import { StoreProvider } from 'app/providers/StoreProvider';
 import App from './App/App';
 
 import 'shared/config/i18nConfig/i18nConfig';
 
 render(
-  <BrowserRouter>
-    <ErrorBoundary useSuspense={false}>
-      <ThemeProvider>
-        <Suspense fallback="Перевод...">
-          <App />
-        </Suspense>
-      </ThemeProvider>
-    </ErrorBoundary>
-  </BrowserRouter>,
+  <StoreProvider>
+    <BrowserRouter>
+      <ErrorBoundary useSuspense={false}>
+        <ThemeProvider>
+          <Suspense fallback="Перевод...">
+            <App />
+          </Suspense>
+        </ThemeProvider>
+      </ErrorBoundary>
+    </BrowserRouter>
+  </StoreProvider>,
   document.getElementById('root'),
 );
