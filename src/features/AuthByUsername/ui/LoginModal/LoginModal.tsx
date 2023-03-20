8756c1f8f9b4c8d36c277f26c8580ed1462ab4ed
Малков Сергей Юрьевич <smalkov@phoenixit.ru>
2023-03-20 13:26:07 +0300

    delete i18n translate hook from login modal

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,5 +1,4 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
 
@@ -12,8 +11,6 @@ interface LoginModalProps {
 }
 
 export const LoginModal = ({ isOpen, onClose, className }: LoginModalProps) => {
-  const { t } = useTranslation();
-
   const classes = classNames(cls.LoginModal, {}, [className]);
 
   return (
