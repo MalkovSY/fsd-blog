0acaeb8efc6e24b29c98f3a0342d24d2fb27ca29
Малков Сергей Юрьевич <smalkov@phoenixit.ru>
2023-03-20 15:28:47 +0300

    create custom input

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,7 +1,9 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
+import { Input } from 'shared/ui/Input/Input';
 
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { useCallback, useState } from 'react';
 import cls from './LoginForm.modules.scss';
 
 interface LoginFormProps {
@@ -9,22 +11,32 @@ interface LoginFormProps {
 }
 
 export const LoginForm = ({ className }: LoginFormProps) => {
+  const [usernameValue, setUsernameValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
   const { t } = useTranslation();
+
+  const changeUsernameHandler = useCallback((value) => setUsernameValue(value), []);
+
+  const changePasswordHandler = useCallback((value) => setPasswordValue(value), []);
 
   const classes = classNames(cls.LoginForm, {}, [className]);
 
   return (
     <div className={classes}>
-      <input
-        type="text"
+      <Input
+        value={usernameValue}
+        onChange={changeUsernameHandler}
+        placeholder={t('Логин')}
         className={cls.input}
       />
-      <input
-        type="text"
+      <Input
+        value={passwordValue}
+        onChange={changePasswordHandler}
+        placeholder={t('Пароль')}
         className={cls.input}
       />
       <Button
-        className={cls.submitBtn}
+        className={cls.loginBtn}
         theme={ButtonTheme.BACKGROUND_INVERTED}
       >
         {t('Войти')}
---
+++ src/shared/ui/Input/Input.tsx
@@ -0,0 +1,70 @@
+import {
+  ChangeEvent,
+  HTMLInputTypeAttribute,
+  InputHTMLAttributes,
+  memo,
+  useCallback,
+  useState,
+} from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import cls from './Input.modules.scss';
+
+type InputExtendedProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>;
+
+interface InputProps extends InputExtendedProps {
+  value?: string;
+  onChange?: (value: string) => void;
+  type?: HTMLInputTypeAttribute;
+  placeholder?: string;
+  className?: string;
+}
+
+const INPUT_CARET_WIDTH = 9;
+
+const InputProto = ({
+  value, onChange, type = 'text', placeholder, className, ...otherProps
+}: InputProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [caretPosition, setCaretPosition] = useState(0);
+
+  const changeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    onChange?.(e.target.value);
+  }, [onChange]);
+
+  const selectHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setCaretPosition(e.target.selectionStart || 0);
+  }, []);
+
+  const focusHandler = useCallback(() => setIsFocused(true), []);
+
+  const blurHandler = useCallback(() => setIsFocused(false), []);
+
+  const classes = classNames(cls.Input, {}, [className]);
+
+  return (
+    <div className={classes}>
+      {placeholder ? <span className={cls.placeholder}>{`${placeholder}>`}</span> : null}
+      <div className={cls.InputWrapper}>
+        <input
+          value={value}
+          onChange={changeHandler}
+          onSelect={selectHandler}
+          onFocus={focusHandler}
+          onBlur={blurHandler}
+          type={type}
+          className={cls.input}
+          {...otherProps}
+        />
+        {isFocused ? (
+          <span
+            style={{ left: caretPosition * INPUT_CARET_WIDTH }}
+            className={cls.caret}
+          />
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export const Input = memo(InputProto);
