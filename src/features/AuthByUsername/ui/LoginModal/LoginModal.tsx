9f05d6fa29bb899ebda88336ca292e6a856ca430
Малков Сергей Юрьевич <smalkov@phoenixit.ru>
2023-03-21 09:21:21 +0300

    add husky precommit, fix stylelint errors

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -2,8 +2,6 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
 
-import cls from './LoginModal.modules.scss';
-
 interface LoginModalProps {
   isOpen: boolean;
   onClose: () => void;
@@ -11,7 +9,7 @@ interface LoginModalProps {
 }
 
 export const LoginModal = ({ isOpen, onClose, className }: LoginModalProps) => {
-  const classes = classNames(cls.LoginModal, {}, [className]);
+  const classes = classNames('', {}, [className]);
 
   return (
     <Modal className={classes} isOpen={isOpen} onClose={onClose} lazy>
