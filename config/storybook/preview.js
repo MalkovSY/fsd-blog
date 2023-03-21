e5ea34d2fc3c103bd819b95ca9aad84ccecd58a9
Малков Сергей Юрьевич <smalkov@phoenixit.ru>
2023-03-21 13:43:46 +0300

    create TranslationDecorator for storybook

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,6 +1,7 @@
 import { addDecorator } from '@storybook/react';
-import { Theme } from 'shared/lib/useTheme/ThemContext';
-import { RouterDecorator } from 'shared/config/storybook/RouterDecorator/RouterDecorator';
+import { Theme } from '../../src/shared/lib/useTheme/ThemContext';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+import { TranslationDecorator } from '../../src/shared/config/storybook/TranslationDecorator/TranslationDecorator';
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 
@@ -17,3 +18,4 @@ export const parameters = {
 addDecorator(StyleDecorator);
 addDecorator(ThemeDecorator(Theme.DEFAULT));
 addDecorator(RouterDecorator);
+addDecorator(TranslationDecorator);
