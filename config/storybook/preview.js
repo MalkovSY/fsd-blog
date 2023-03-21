a4cd3742f78ae9c5ad7d2d79cc1f482dccb98000
Малков Сергей Юрьевич <smalkov@phoenixit.ru>
2023-03-21 13:55:45 +0300

    Delete translation decorator

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,7 +1,6 @@
 import { addDecorator } from '@storybook/react';
 import { Theme } from '../../src/shared/lib/useTheme/ThemContext';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
-import { TranslationDecorator } from '../../src/shared/config/storybook/TranslationDecorator/TranslationDecorator';
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 
@@ -18,4 +17,3 @@ export const parameters = {
 addDecorator(StyleDecorator);
 addDecorator(ThemeDecorator(Theme.DEFAULT));
 addDecorator(RouterDecorator);
-addDecorator(TranslationDecorator);
