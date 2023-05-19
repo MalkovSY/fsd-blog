3072b07380ea2b53f6702f504885cb1f032ffb1b
Малков Сергей Юрьевич <smalkov@phoenixit.ru>
2023-05-19 20:05:57 +0300

    create story for LoginForm component

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -1,4 +1,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'shared/lib/useTheme/ThemContext';
 import { LoginForm } from './LoginForm';
 
 export default {
@@ -8,4 +11,10 @@ export default {
 
 const Template: ComponentStory<typeof LoginForm> = (args) => <LoginForm {...args} />;
 
-export const Primary = Template.bind({});
+export const Normal = Template.bind({});
+Normal.args = {};
+Normal.decorators = [StoreDecorator()];
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [StoreDecorator(), ThemeDecorator(Theme.DARK)];
