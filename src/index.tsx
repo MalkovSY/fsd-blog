e7aa29f1c58a308727bdeda1b8404bfeae2486ad
Малков Сергей Юрьевич <smalkov@phoenixit.ru>
2023-07-27 13:50:27 +0300

    delete ts-ignore from index.tsx TS 5

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { StoreProvider } from 'app/providers/StoreProvider';
-// @ts-ignore
-import App from './App/App';
+import App from './app/App';
 
 import 'shared/config/i18nConfig/i18nConfig';
 
